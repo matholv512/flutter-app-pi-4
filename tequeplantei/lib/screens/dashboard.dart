135b944938653f6454e1330de23f4376d499956d
Matheus Oliveira <matholv512@outlook.com>
2022-11-12 17:51:29 -0300

    (12/11/22) - consulta ao firebase implementada

--- tequeplantei/lib/screens/dashboard.dart
+++ tequeplantei/lib/screens/dashboard.dart
@@ -1,97 +1,91 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, duplicate_ignore, use_key_in_widget_constructors, library_private_types_in_public_api
+import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
-import 'package:flutter/material.dart';
+
 
 class Dashboard extends StatefulWidget {
-  const Dashboard({Key? key}) : super(key: key);
   @override
-  // _DashboardState createState() => _DashboardState();
-  State<Dashboard> createState() => _DashboardState();
+  _DashboardState createState() => _DashboardState();
 }
 
 class _DashboardState extends State<Dashboard> {
-  Query dbRef = FirebaseDatabase.instance.ref().child('ProjectData');
-
-  Widget listItem({required Map data}) {
+  @override
+  Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+    final ref = FirebaseDatabase.instance.ref().child("ProjectData"); // implementar o onChildAdded
     return SafeArea(
       child: Stack(
         children: <Widget>[
           Scaffold(
             backgroundColor: Colors.white,
             appBar: PreferredSize(
-              preferredSize: Size(MediaQuery.of(context).size.width, 100.0),
-              child: Container(
-                alignment: Alignment.topLeft,
-                height: 100.0,
-                color: Colors.green,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                        icon: Icon(
-                          Icons.menu,
-                          color: Colors.white,
+                preferredSize:
+                    Size(MediaQuery.of(context).size.width, 100.0),
+                  child: Container(
+                    alignment: Alignment.topLeft,
+                    height: 100.0,
+                    color: Colors.green,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                            icon: Icon(
+                              Icons.menu,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {}),
+                        Text(
+                          'TéquePlantei',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
-                        onPressed: () {}),
-                    Text(
-                      'Dashboard',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                      ),
+                        IconButton(
+                            icon: Icon(
+                              Icons.settings,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {})
+                      ],
                     ),
-                    IconButton(
-                        icon: Icon(
-                          Icons.settings,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {})
-                  ],
+                  ),
+                ),
+            body: FirebaseAnimatedList(
+          query: ref,
+          itemBuilder: (context, snapshot, animation, index) {
+            return Container(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 80.0),
+                child: ListTile(
+                  title: Text(
+                    snapshot.value.toString(),
+                    style: const TextStyle(
+                        fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
-            body: Container(
-              // color: Colors.green,
-              height: double.infinity,
-              width: double.infinity,
-              // alignment: Alignment.topLeft,
-              margin: EdgeInsets.only(top: 100.0),
-              // 68.0
-              child: Column(children: [
-                Text(
-                  data['temperatura'],
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-                ),
-                Text(
-                  data['umidadeAr'],
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-                ),
-                Text(
-                  data['umidadeSolo'],
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-                ),
-              ]),
-            ),
+            );
+          }),
+          
           ),
           Positioned(
-            // ignore: sort_child_properties_last
             child: Container(
               child: Container(
-                  // ignore: sort_child_properties_last
-                  child: Image.asset(
-                    'images/plant2.png',
-                    width: 110,
-                  ),
-                  decoration: BoxDecoration(
-                      // borderRadius: BorderRadius.circular(60),
-                      color: Colors.white,
-                      border: Border.all(
-                        color: Colors.green,
-                        width: 2.5,
-                      ))),
+                child: Image.asset(
+                'images/plant2.png',
+                width: 110,
+              ),
+              decoration: BoxDecoration(
+                // borderRadius: BorderRadius.circular(60),
+                color: Colors.white,
+                border: Border.all(
+                  color: Colors.green,
+                  width: 2.5,
+                )
+                )
+              ),
             ),
             left: (size.width / 2) - 55,
             top: 48,
@@ -99,25 +93,5 @@ class _DashboardState extends State<Dashboard> {
         ],
       ),
     );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-        body: Container(
-      height: double.infinity,
-      width: double.infinity,
-      child: FirebaseAnimatedList(
-        query: dbRef,
-        itemBuilder: (BuildContext context, DataSnapshot snapshot,
-            Animation<double> animation, int index) {
-          Map data = snapshot.value as Map;
-          data['key'] = snapshot.key;
-
-          return listItem(data: data);
-        },
-      ),
-    ));
   }
 }
